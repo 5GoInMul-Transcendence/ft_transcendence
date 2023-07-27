b51fc4c9c2da056b6530e567c6c93db4dd7fd97c
donghyuk <reg0145@naver.com>
2023-07-28 07:57:39 +0900

    [Feat] ConfigModule 추가

--- back/web-app/src/app.module.ts
+++ back/web-app/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import {SmsModule} from './auth/sms/sms.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot( { isGlobal:true })],
   controllers: [AppController],
   providers: [AppService],
 })
