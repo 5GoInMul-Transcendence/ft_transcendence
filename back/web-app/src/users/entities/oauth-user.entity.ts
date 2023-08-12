657d1ec0b43055633fd85d4a71039d7590ec5bde
donghyuk <reg0145@naver.com>
2023-08-12 16:51:51 +0900

    [Refactor] Users enum들을 enums 폴더로 묶음

--- back/web-app/src/users/entities/oauth-user.entity.ts
+++ back/web-app/src/users/entities/oauth-user.entity.ts
@@ -1,4 +1,4 @@
-import { ServiceProvider } from "src/users/service-provider.enum";
+import { ServiceProvider } from "src/users/enums/service-provider.enum";
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./user.entity";
 
