67ea87f49276644b11c27b6998324eea5d1c672c
donghyuk <reg0145@naver.com>
2023-08-12 16:24:08 +0900

    [Feat] MemoryUserService에 필요한 entity 구현
    - block, friend, follwer 폴더를 생성했습니다.
    - block은 아직 entity밖에 없으나 나중에 '차단 관련 API'를 구현할 때 controller, service가 생성될 예정입니다.
    - friend 또한 마찬가지로 '친구 관련 API'를 구현할 때 controller, service가 생성될 예정입니다.
    - 각 entity는 ERD 설계와 동일하게 칼럼 옵션을 구성하였습니다.

---
+++ back/web-app/src/block/block.entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('block')
+export class Block {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ type: 'integer', name: 'user_id' })
+  userId: number;
+  @Column({ type: 'integer', array: true, default: [] })
+  blocks: number[];
+}
