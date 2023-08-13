dd4b680e503a856db6913f250ea58fd9276df5ab
donghyuk <reg0145@naver.com>
2023-08-14 08:48:00 +0900

    [Feat] UserModule 수정 #48
    - MeController를 포함시켰습니다.
    - MeController에서 필요한 MemoryUserService,MemoryUserProvider를 providers로 포함시켰습니다.
    - MemoryUserProvider에서 필요한 Friend, Follower, Block 레퍼지토리를 TypeormModule에 포함시켰습니다.

--- back/web-app/src/users/user/user.module.ts
+++ back/web-app/src/users/user/user.module.ts
@@ -4,6 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { OauthUser } from '../entities/oauth-user.entity';
 import { MemberUser } from '../entities/member-user.entity';
+import { MeController } from '../me/me.controller';
+import { MemoryUserService } from '../memoryuser/memory-user.service';
+import { MemoryUserProvider } from '../memoryuser/memory-user.provider';
+import { Block } from '../../block/block.entity';
+import { Follower } from '../../friend/entities/follower.entity';
+import { Friend } from '../../friend/entities/friend.entity';
 
 @Module({
   imports: [
@@ -11,9 +17,13 @@ import { MemberUser } from '../entities/member-user.entity';
       User,
       OauthUser,
       MemberUser,
+      Friend,
+      Follower,
+      Block,
     ]),
   ],
-  providers: [UserService],
+  controllers: [MeController],
+  providers: [UserService, MemoryUserService, MemoryUserProvider],
   exports: [UserService],
 })
 export class UserModule {}
