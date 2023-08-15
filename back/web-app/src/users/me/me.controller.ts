ba97228ccfa31c6452f6aba7d789e2aa6a4c43c0
donghyuk <reg0145@naver.com>
2023-08-15 22:50:04 +0900

    [Fix] 연관없는 소스가 푸시된 부분 수정 #84

--- back/web-app/src/users/me/me.controller.ts
+++ back/web-app/src/users/me/me.controller.ts
@@ -1,13 +1,10 @@
-import { Controller, Get, Param, Session } from '@nestjs/common';
+import { Controller, Get, Session } from '@nestjs/common';
 import { MemoryUserService } from '../memoryuser/memory-user.service';
 import { Builder } from 'builder-pattern';
 import { FindUserDto } from '../memoryuser/dto/find-user.dto';
 import { GetUserProfileResDto } from './dto/get-user-profile-res.dto';
 import { GameRecordDto } from './dto/game-record.dto';
 import { GetUserProfileDetailsResDto } from './dto/get-user-profile-details-res.dto';
-import { GetUserByNicknameReqDto } from './dto/get-user-by-nickname-req.dto';
-import { FindUserByNicknameDto } from '../memoryuser/dto/find-user-by-nickname.dto';
-import { GetUserByNicknameResDto } from './dto/get-user-by-nickname-res.dto';
 
 @Controller('me')
 export class MeController {
@@ -46,31 +43,6 @@ export class MeController {
       .mail(me.mail)
       .phone(me.phone)
       .twoFactor(me.twoFactor)
-      .build();
-  }
-
-  @Get('users/:nickname')
-  getUserByNickname(@Session() session, @Param() dto: GetUserByNicknameReqDto) {
-    const me = this.memoryUserService.findUser(
-      Builder(FindUserDto).userId(session.userId).build(),
-    );
-    const findUser = this.memoryUserService.findUserByNickname(
-      Builder(FindUserByNicknameDto).nickname(dto.nickname).build(),
-    );
-
-    const gameRecodeDto = Builder(GameRecordDto)
-      .win(10)
-      .loss(10)
-      .ladderLevel(10)
-      .achievement(['0123456789abcdef', '0123456789abcdef'])
-      .build();
-    return Builder(GetUserByNicknameResDto)
-      .id(findUser.id)
-      .nickname(findUser.nickname)
-      .avatar(findUser.avatar)
-      .gameRecord(gameRecodeDto)
-      .isFriend(me.friends.has(findUser.id))
-      .isisBlock(me.blocks.has(findUser.id))
       .build();
   }
 }
