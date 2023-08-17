a728d63ddebe913c8b9dcb1281849c73df9bc4bc
pearpearB <smilyoung@gmail.com>
2023-08-17 18:37:48 +0900

    [Feat] 폰 인증 모달 구현 #78

--- front/src/component/Modal/AuthPhone.tsx
+++ front/src/component/Modal/AuthPhone.tsx
@@ -1,3 +1,69 @@
+import { useState } from 'react';
+import useInput from '@/hooks/useInput';
+import Input from '@/component/Input';
+import Button from '@/component/Buttons/Button';
+import InvalidMsg from './InvalidMsg';
+import styled from 'styled-components';
+
 export default function AuthPhone() {
-  return <div>AUTHPhone</div>;
+  const [phone, , onChangePhone] = useInput('');
+  const [code, , onChangeCode] = useInput('');
+  const [invalidPhoneMsg, setInvalidPhoneMsg] = useState<string>('');
+  const [invalidCodeMsg, setInvalidCodeMsg] = useState<string>('');
+
+  const sendPhoneHandler = async () => {
+    const regExp = /\d{2,3}-\d{3,4}-\d{4}/g;
+    if (regExp.test(phone) === false) {
+      setInvalidPhoneMsg(() => 'Wrong mail address');
+      return;
+    }
+    setInvalidPhoneMsg(() => '');
+    /* todo: 폰 data 요청, response에 따라 invalidPhoneMsg설정 */
+  };
+
+  const authPhoneHandler = async () => {
+    /* todo: 인증 data 요청, response에 따라 invalidCodeMsg 설정 */
+    setInvalidCodeMsg(() => 'Wrong Code');
+  };
+
+  return (
+    <>
+      <InputButtonWrapper>
+        <Input
+          label='Phone'
+          type='text'
+          value={phone}
+          onChange={onChangePhone}
+          maxLength={11}
+        />
+        <Button
+          text='send'
+          color='green'
+          width='8rem'
+          onClick={sendPhoneHandler}
+        />
+      </InputButtonWrapper>
+      <InvalidMsg text={invalidPhoneMsg} />
+      <Input
+        label='Code'
+        type='text'
+        value={code}
+        onChange={onChangeCode}
+        maxLength={100}
+      />
+      <InvalidMsg text={invalidCodeMsg} />
+      <Button text='submit' color='green' onClick={authPhoneHandler} />
+    </>
+  );
 }
+
+const InputButtonWrapper = styled.div`
+  ${({ theme }) => theme.flex.spaceBetween};
+  input {
+    width: 92%;
+  }
+  button {
+    margin-top: calc(20px + 1rem);
+    margin-bottom: 1rem;
+  }
+`;
