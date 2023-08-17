129be413d32c2d09793fdcd129dd32f4e15c9434
pearpearB <smilyoung@gmail.com>
2023-08-17 18:31:30 +0900

    [Feat] 이메일 인증 모달 구현 #78

--- front/src/component/Modal/AuthMail.tsx
+++ front/src/component/Modal/AuthMail.tsx
@@ -1,3 +1,70 @@
+import { useState } from 'react';
+import useInput from '@/hooks/useInput';
+import Input from '@/component/Input';
+import Button from '@/component/Buttons/Button';
+import InvalidMsg from './InvalidMsg';
+import styled from 'styled-components';
+
 export default function AuthMail() {
-  return <div>AUTHMail</div>;
+  const [mail, , onChangeMail] = useInput('');
+  const [code, , onChangeCode] = useInput('');
+  const [invalidMailMsg, setInvalidMailMsg] = useState<string>('');
+  const [invalidCodeMsg, setInvalidCodeMsg] = useState<string>('');
+
+  const sendMailHandler = async () => {
+    const regExp =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if (regExp.test(mail) === false) {
+      setInvalidMailMsg(() => 'Wrong mail address');
+      return;
+    }
+    setInvalidMailMsg(() => '');
+    /* todo: 메일 data 요청, response에 따라 invalidMailMsg 설정 */
+  };
+
+  const authMailHandler = async () => {
+    /* todo: 인증 data 요청, response에 따라 invalidCodeMsg 설정 */
+    setInvalidCodeMsg(() => 'Wrong Code');
+  };
+
+  return (
+    <>
+      <InputButtonWrapper>
+        <Input
+          label='Mail'
+          type='email'
+          value={mail}
+          onChange={onChangeMail}
+          maxLength={320}
+        />
+        <Button
+          text='send'
+          color='green'
+          width='8rem'
+          onClick={sendMailHandler}
+        />
+      </InputButtonWrapper>
+      <InvalidMsg text={invalidMailMsg} />
+      <Input
+        label='Code'
+        type='text'
+        value={code}
+        onChange={onChangeCode}
+        maxLength={100}
+      />
+      <InvalidMsg text={invalidCodeMsg} />
+      <Button text='submit' color='green' onClick={authMailHandler} />
+    </>
+  );
 }
+
+const InputButtonWrapper = styled.div`
+  ${({ theme }) => theme.flex.spaceBetween};
+  input {
+    width: 92%;
+  }
+  button {
+    margin-top: calc(20px + 1rem);
+    margin-bottom: 1rem;
+  }
+`;
