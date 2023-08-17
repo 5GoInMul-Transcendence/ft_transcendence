febf3a5eb7885035c9bbadd22c53c8845b364451
pearpearB <smilyoung@gmail.com>
2023-08-17 12:17:20 +0900

    [Feat] 닉네임변경 모달 구현 #78

--- front/src/component/Modal/SetNick.tsx
+++ front/src/component/Modal/SetNick.tsx
@@ -1,3 +1,33 @@
+import { useState } from 'react';
+import useInput from '@/hooks/useInput';
+import Input from '@/component/Input';
+import Button from '@/component/Buttons/Button';
+import InvalidMsg from './InvalidMsg';
+
 export default function SetNick() {
-  return <div>SETNick</div>;
+  const [keyword, , onChangeKeyword] = useInput('');
+  const [inValidMsg, setInValidMsg] = useState<string>('');
+
+  const setNickHandler = async () => {
+    /* todo: 검색 data 요청, response에 따라 inValidMsg 설정 
+		예시)
+		'Nickname already exist'
+		...
+		*/
+    setInValidMsg(() => 'Nickname already exist');
+  };
+
+  return (
+    <>
+      <Input
+        label='Nickname'
+        type='text'
+        value={keyword}
+        onChange={onChangeKeyword}
+        maxLength={12}
+      />
+      <InvalidMsg text={inValidMsg} />
+      <Button text='submit' color='green' onClick={setNickHandler} />
+    </>
+  );
 }
