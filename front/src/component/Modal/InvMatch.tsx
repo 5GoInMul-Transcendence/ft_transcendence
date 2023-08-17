796fdc309ede29296d29b60ca94a03cdb655b8c9
pearpearB <smilyoung@gmail.com>
2023-08-17 11:45:43 +0900

    [Feat] 게임초대 모달 구현 #78

--- front/src/component/Modal/InvMatch.tsx
+++ front/src/component/Modal/InvMatch.tsx
@@ -1,3 +1,21 @@
+import styled from 'styled-components';
+import Button from '../Buttons/Button';
+
 export default function InvMatch() {
-  return <div>INVMatch</div>;
+  const invClassicMatchHandler = async () => {};
+  const invShortMatchHandler = async () => {};
+
+  return (
+    <Wrapper>
+      <Button text='Classic' color='green' onClick={invClassicMatchHandler} />
+      <Button text='Short' color='green' onClick={invShortMatchHandler} />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  ${({ theme }) => theme.flex.centerColumn};
+  justify-content: space-between;
+  width: 100%;
+  height: 8rem;
+`;
