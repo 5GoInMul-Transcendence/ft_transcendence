4de8dadc6d206399ebd632db52e32530158bee11
donghyuk <reg0145@naver.com>
2023-08-20 19:54:35 +0900

    [Feat] FriendController 수정
    - @Controller() 데코레이터 핸들러 경로 수정
      - 친구 리스트 조회(/friendlist) 핸들러를 추가하기 위해 공백으로 수정하였습니다.
    
    - getFriendsInfo()
      - 친구 리스트 조회 요청을 처리하는 핸들러를 구현하였습니다.

--- back/web-app/src/friend/friend.controller.ts
+++ back/web-app/src/friend/friend.controller.ts
@@ -1,20 +1,28 @@
-import { Body, Controller, Post, Session } from '@nestjs/common';
+import { Body, Controller, Post, Get, Session } from '@nestjs/common';
 import { AddFriendReqDto } from './dto/add-friend-req.dto';
 import { AddFriendDto } from './dto/add-friend.dto';
 import { Builder } from 'builder-pattern';
 import { FriendService } from './friend.service';
+import { GetFriendsInfoDto } from './dto/get-friends-info.dto';
 
-@Controller('friend')
+@Controller()
 export class FriendController {
   constructor(private friendService: FriendService) {}
 
-  @Post()
+  @Post('friend')
   addFriend(@Session() session, @Body() dto: AddFriendReqDto) {
     return this.friendService.addFriend(
       Builder(AddFriendDto)
         .userId(session.userId)
         .nickname(dto.nickname)
         .build(),
+    );
+  }
+
+  @Get('friendlist')
+  getFriendsInfo(@Session() session) {
+    return this.friendService.getFriendsInfo(
+      Builder(GetFriendsInfoDto).userId(session.userId).build(),
     );
   }
 }
