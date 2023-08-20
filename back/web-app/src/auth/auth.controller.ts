3cf543ae8d59f7cff1cab5e9fb0acfaf26d4ed8f
donghyuk <reg0145@naver.com>
2023-08-20 13:30:21 +0900

    [Feat] authControlloer 수정 #98
    - authPhone()
      - '핸드폰 인증코드 전송' API 요청을 받는 핸들러를 구현하였습니다.

--- back/web-app/src/auth/auth.controller.ts
+++ back/web-app/src/auth/auth.controller.ts
@@ -3,6 +3,8 @@ import { Builder } from 'builder-pattern';
 import { AuthService } from './auth.service';
 import { AuthMailDto } from './dto/auth-mail.dto';
 import { AuthMailReqDto } from './dto/auth-mail-req.dto';
+import { AuthPhoneReqDto } from './dto/auth-phone-req.dto';
+import { AuthPhoneDto } from './dto/auth-phone.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,6 +14,13 @@ export class AuthController {
   authMail(@Session() session, @Body() dto: AuthMailReqDto) {
     this.authService.authMail(
       Builder(AuthMailDto).userId(session.userId).mail(dto.mail).build(),
+    );
+  }
+
+  @Post('phone')
+  authPhone(@Session() session, @Body() dto: AuthPhoneReqDto) {
+    this.authService.authPhone(
+      Builder(AuthPhoneDto).userId(session.userId).phone(dto.phone).build(),
     );
   }
 }
