9284ff1978dfb94e4430dee57475e5cb20013967
donghyuk <reg0145@naver.com>
2023-08-20 13:25:21 +0900

    [Feat] auth 모듈 수정 #98
    - imports
      - SmsModule: smsService를 사용하여 문자전송을 보내고자 포함시키게 되었습니다.

--- back/web-app/src/auth/auth.module.ts
+++ back/web-app/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MailModule } from './mail/mail.module';
+import { SmsModule } from './sms/sms.module';
 
 @Module({
-  imports: [MailModule],
+  imports: [MailModule, SmsModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
