89f53fb983ec41f215492e356699ad325603ccc6
donghyuk <reg0145@naver.com>
2023-08-20 13:28:10 +0900

    [Feat] authService 수정 #98
    - authPhone() 추가
      - smsService를 통해 문자전송을 보내고 인증코드를 받습니다.
      - 전달받은 인증코드와 현재 인증방식(phone), 그리고 인증성공시 수정할 프로필 정보(핸드폰 번호)를 메모리에 저장합니다.

--- back/web-app/src/auth/auth.service.ts
+++ back/web-app/src/auth/auth.service.ts
@@ -4,12 +4,17 @@ import { MailService } from './mail/mail.service';
 import { AuthMailDto } from './dto/auth-mail.dto';
 import { Builder } from 'builder-pattern';
 import { TwoFactorStatus } from '../users/enums/twoFactor-status.enum';
+import { AuthPhoneDto } from './dto/auth-phone.dto';
+import { SmsService } from './sms/sms.service';
 
 @Injectable()
 export class AuthService {
   private authInfos: Map<number, AuthInfo>;
 
-  constructor(private mailService: MailService) {
+  constructor(
+    private mailService: MailService,
+    private smsService: SmsService,
+  ) {
     this.authInfos = new Map<number, AuthInfo>();
   }
 
@@ -21,6 +26,18 @@ export class AuthService {
       Builder(AuthInfo)
         .twoFactor(TwoFactorStatus.MAIL)
         .modifyValue(dto.mail)
+        .code(code)
+        .build(),
+    );
+  }
+  async authPhone(dto: AuthPhoneDto) {
+    const code = await this.smsService.sendSMS(dto.phone);
+
+    this.authInfos.set(
+      dto.userId,
+      Builder(AuthInfo)
+        .twoFactor(TwoFactorStatus.PHONE)
+        .modifyValue(dto.phone)
         .code(code)
         .build(),
     );
