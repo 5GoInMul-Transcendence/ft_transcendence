f8dbc616e526585a9bcdc9f1f6fa9886c60008f6
donghyuk <reg0145@naver.com>
2023-08-20 17:20:34 +0900

    [Feat] AuthModule 수정 #99
    - imports
      - UserModule 추가: 인증 코드 확인 후 유저의 정보를 업데이트하기위해(UserService를 사용하기 위해) 포함하게 되었습니다.

--- back/web-app/src/auth/auth.module.ts
+++ back/web-app/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MailModule } from './mail/mail.module';
+import { UserModule } from '../users/user/user.module';
 
 @Module({
-  imports: [MailModule],
+  imports: [MailModule, UserModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
