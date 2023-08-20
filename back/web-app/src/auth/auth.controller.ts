049fbdeaf8144830aa928d76b34ce6af5976fd06
donghyuk <reg0145@naver.com>
2023-08-20 17:28:21 +0900

    [Feat] AuthController 수정 #99
    - checkAuthCode()
      - '인증코드 확인'요청을 받는 핸들러를 구현하였습니다.

--- back/web-app/src/auth/auth.controller.ts
+++ back/web-app/src/auth/auth.controller.ts
@@ -3,6 +3,8 @@ import { Builder } from 'builder-pattern';
 import { AuthService } from './auth.service';
 import { AuthMailDto } from './dto/auth-mail.dto';
 import { AuthMailReqDto } from './dto/auth-mail-req.dto';
+import { CheckAuthCodeReqDto } from './dto/check-auth-code-req.dto';
+import { CheckAuthCodeDto } from './dto/check-auth-code.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,6 +14,13 @@ export class AuthController {
   authMail(@Session() session, @Body() dto: AuthMailReqDto) {
     this.authService.authMail(
       Builder(AuthMailDto).userId(session.userId).mail(dto.mail).build(),
+    );
+  }
+
+  @Post()
+  checkAuthCode(@Session() session, @Body() dto: CheckAuthCodeReqDto) {
+    this.authService.checkAuthCode(
+      Builder(CheckAuthCodeDto).userId(session.userId).code(dto.code).build(),
     );
   }
 }
