7b4121a11253fb130cff2df5a65cb266b8e8d8b5
donghyuk <reg0145@naver.com>
2023-08-22 21:14:49 +0900

    [Refactor] Conflict 수정

--- back/web-app/src/users/user/user.controller.ts
+++ back/web-app/src/users/user/user.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Get, Param, Session } from "@nestjs/common";
-import { GetUserProfileByNicknameReqDto } from "./dto/get-user-profile-by-nickname-req.dto";
-import { FindUserDto } from "../memoryuser/dto/find-user.dto";
-import { FindUserByNicknameDto } from "../memoryuser/dto/find-user-by-nickname.dto";
-import { GameRecordDto } from "../me/dto/game-record.dto";
-import { GetUserProfileByNicknameResDto } from "./dto/get-user-profile-by-nickname-res.dto";
-import { Builder } from "builder-pattern";
-import { MemoryUserService } from "../memoryuser/memory-user.service";
+import { Controller, Get, Param, Session } from '@nestjs/common';
+import { GetUserProfileByNicknameReqDto } from './dto/get-user-profile-by-nickname-req.dto';
+import { FindUserDto } from '../memoryuser/dto/find-user.dto';
+import { FindUserByNicknameDto } from '../memoryuser/dto/find-user-by-nickname.dto';
+import { GameRecordDto } from '../me/dto/game-record.dto';
+import { GetUserProfileByNicknameResDto } from './dto/get-user-profile-by-nickname-res.dto';
+import { Builder } from 'builder-pattern';
+import { MemoryUserService } from '../memoryuser/memory-user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private memoryUserService: MemoryUserService) {}
-  
+
   @Get(':nickname')
   getUserProfileByNickname(
     @Session() session,
@@ -22,7 +22,7 @@ export class UserController {
     const findUser = this.memoryUserService.findUserByNickname(
       Builder(FindUserByNicknameDto).nickname(dto.nickname).build(),
     );
-    
+
     const gameRecodeDto = Builder(GameRecordDto)
       .win(10)
       .loss(10)
