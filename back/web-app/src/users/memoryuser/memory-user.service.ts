6161c0290805992af5927e8da9b419543a0860d5
donghyuk <reg0145@naver.com>
2023-08-22 11:09:15 +0900

    [Feat] MemoryUserService 수정 #115
    - checkAvailableTwoFactor() 메서드 명은 'Available 이용 가능한'이라는 단어를 넣어 만들었습니다.
    - mail을 선택하였을 경우 mail이 null 유무를 확인합니다.
    - phone을 선태갛였을 경우 phone이 null인지 확인합니다.

--- back/web-app/src/users/memoryuser/memory-user.service.ts
+++ back/web-app/src/users/memoryuser/memory-user.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { FindUserDto } from './dto/find-user.dto';
 import { MemoryUserProvider } from './memory-user.provider';
 import { MemoryUser } from './memory-user';
@@ -12,6 +12,9 @@ import { DeleteUserFriendDto } from './dto/delete-user-friend.dto';
 import { UserDto } from '../user/dto/user.dto';
 import { Builder } from 'builder-pattern';
 import _ from 'lodash';
+import { CheckAvailableTwofactorDto } from './dto/check-available-twofactor.dto';
+import { TwoFactorStatus } from '../enums/twoFactor-status.enum';
+
 @Injectable()
 export class MemoryUserService {
   constructor(private memoryUsers: MemoryUserProvider) {}
@@ -79,5 +82,18 @@ export class MemoryUserService {
   deleteUserFriend(dto: DeleteUserFriendDto) {
     const user = this.memoryUsers.get(dto.userId);
     user.friends.delete(dto.friendId);
+  }
+
+  checkAvailableTwoFactor(dto: CheckAvailableTwofactorDto) {
+    const user = this.memoryUsers.get(dto.userId);
+
+    if (dto.twoFactor == TwoFactorStatus.MAIL && user.mail == null) {
+      throw new HttpException('잘못된 요청입니다.', HttpStatus.OK);
+    }
+    if (dto.twoFactor == TwoFactorStatus.PHONE && user.phone == null) {
+      throw new HttpException('잘못된 요청입니다.', HttpStatus.OK);
+    }
+
+    return;
   }
 }
