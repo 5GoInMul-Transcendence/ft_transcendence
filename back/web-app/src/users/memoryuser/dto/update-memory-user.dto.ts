2f7b873756270ff7fd2a9e17362fc2e464ab961d
donghyuk <reg0145@naver.com>
2023-08-24 12:53:36 +0900

    [Refactor] UpdateMemoryUserDto 수정 #133
    - 메모리 유저의 업데이트 항목으로 status를 추가하였습니다.

--- back/web-app/src/users/memoryuser/dto/update-memory-user.dto.ts
+++ back/web-app/src/users/memoryuser/dto/update-memory-user.dto.ts
@@ -1,9 +1,12 @@
+import { UserStatus } from '../../enums/user-status.enum';
+
 export class UpdateMemoryUserDto {
   readonly userId: number;
   nickname: string;
   avatar: string;
   mail: string;
   phone: string;
+  status: UserStatus;
 
   constructor() {
     // 클래스 생성자 내부에서 프로퍼티 플래그 설정
