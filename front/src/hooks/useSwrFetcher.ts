548c9c6ca6a0bdcbcbe8289ce26329d89711c4b4
arkingco <arkingco@gmail.com>
2023-09-01 15:49:44 +0900

    feat : add useSwrFetcher undefined case#180

--- front/src/hooks/useSwrFetcher.ts
+++ front/src/hooks/useSwrFetcher.ts
@@ -4,6 +4,10 @@ import fetcher from '@/utils/fetcher';
 const useSwrFetcher = <T>(api: string): T | undefined => {
   const { data } = useSwr(api, fetcher);
 
+  if (data?.resStatus.code !== '0000') return undefined;
+  if (data?.resStatus.code !== '0001') return undefined;
+  if (data?.resStatus.code !== '0002') return undefined;
+
   return data?.data;
 };
 
