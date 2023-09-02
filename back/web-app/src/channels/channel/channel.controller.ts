3482f75e2a63bccde498e57cc8f81498c63bfe3e
brixxt27 <brixxt27@gmail.com>
2023-09-02 22:16:39 +0900

    chore: 사용하지 않는 memory user service 삭제#176

--- back/web-app/src/channels/channel/channel.controller.ts
+++ back/web-app/src/channels/channel/channel.controller.ts
@@ -1,8 +1,6 @@
 import { Body, Controller, Post, Session } from '@nestjs/common';
 import { AddChannelReqDto } from './dto/add-channel-req.dto';
-import { MemoryUserService } from 'src/users/memoryuser/memory-user.service';
 import { Builder } from 'builder-pattern';
-import { FindUserDto } from 'src/users/memoryuser/dto/find-user.dto';
 import { ChannelService } from './channel.service';
 import { CreateChannelReqDto } from './dto/create-channel-req.dto';
 import { CreateLinkChannelToUserReqDto } from './dto/create-link-channel-to-user-req.dto';
@@ -10,7 +8,6 @@ import { CreateLinkChannelToUserReqDto } from './dto/create-link-channel-to-user
 @Controller('channel')
 export class ChannelController {
 	constructor(
-		// private memoryUserService: MemoryUserService,
 		private channelService: ChannelService,
 	) {}
 
@@ -19,9 +16,6 @@ export class ChannelController {
 		@Session() session: Record<string, any>,
 		@Body() addChannelReqDto: AddChannelReqDto, // 채널 name 파이프 구현 필요
 	) {
-		// const me = this.memoryUserService.findUserByUserId(   // 여기에서 굳이 memoryUser 를 가져올 필요가 없다. 이미 AuthModdleware 에서 해당 userId 에 대한 session 을 확인했으니 유효한 유저이다.
-		// 	Builder(FindUserDto).userId(session.userId).build(),
-		// );
 		const {name, mode, password} = addChannelReqDto;
 		const userId = session.userId;
 		const createdChannel = await this.channelService.createChannel(
