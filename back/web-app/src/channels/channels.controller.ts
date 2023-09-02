0a95f37228ad59d1a8ab3d03b736376af304852a
brixxt27 <brixxt27@gmail.com>
2023-09-03 03:05:04 +0900

    refactor: 설계 수정 사항 변경 및 session 객체 타입 명시#147
    * channels.controller
    * me.controller
    - My channels 을 가져오는 API 변경으로 channels controller 로 옮겨짐.
      이후 유지 보수를 고려하여 변경하게 되었다.
    
    * user.controller
    - Recond<stirng, any> 는 typescript 에서 제공하는 도구로써 저장하는 객체
      또는 맵의 key 와 value 의 타입을 명시할 수 있는 도구이다.

--- back/web-app/src/channels/channels.controller.ts
+++ back/web-app/src/channels/channels.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Session } from '@nestjs/common';
 import { ChannelsService } from './channels.service';
+import { UserService } from 'src/users/user/user.service';
 
 @Controller('channels')
 export class ChannelsController {
 	constructor(
 		private channelsService: ChannelsService,
+		private userService: UserService,
 	) {}
 
 	@Get()
 	getAllChannels() {
 		return this.channelsService.getAllChannels();
 	}
+
+  @Get('mine')
+  async getMyChannels(@Session() session: Record<string, any>,) {
+    const userId = session.userId;
+    const user = await this.userService.getUserByUserId(userId);
+
+    user.links.forEach(link => {
+      link.channelId; // messageId 추가 예정, message 와 메시지를 보낸 유저의 nickname 가져오기
+    })
+  }
 }
