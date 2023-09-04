150c6c0a95cdcb365a4538b2a8084a2e24e6d167
donghyuk <reg0145@naver.com>
2023-09-04 15:55:08 +0900

    [Refactor] grpc createGame 핸들러 수정
    - toUpperCase() 사용
      - 소문자로 전달되는 gameMode 값을 대문자로 변경하여 키값으로 만든다.

--- back/game-app/src/game/game.controller.ts
+++ back/game-app/src/game/game.controller.ts
@@ -1,17 +1,21 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
-import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
-import { IGame } from './interface/game.interface';
+import { GameService } from './game.service';
+import { CreateGameDto } from './dto/create-game.dto';
+import { Builder } from 'builder-pattern';
+import { GameMode } from './enums/game-mode.enum';
+import { CreateGameReturnDto } from './dto/create-game-return.dto';
 
 @Controller()
 export class GameController {
+  constructor(private gameService: GameService) {}
+
   @GrpcMethod('GameService', 'createGame')
-  createGame(data: Empty): IGame {
-    const gameInfo = {
-      gameId: 'gameId',
-      p1GameKey: 'p1GameKey',
-      p2GameKey: 'p2GameKey',
-    };
-    return gameInfo;
+  createGame(data: CreateGameDto): CreateGameReturnDto {
+    return this.gameService.createGame(
+      Builder(CreateGameDto)
+        .gameMode(GameMode[data.gameMode.toUpperCase()])
+        .build(),
+    );
   }
 }
