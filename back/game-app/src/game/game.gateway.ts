d1dae063ff7d7d23732c31102931b7df01dde6e6
donghyuk <reg0145@naver.com>
2023-09-04 17:24:50 +0900

    [Feat] handleConnection 수정
    - 유저가 연결되었을 경우 connectGame을 호출합니다.

--- back/game-app/src/game/game.gateway.ts
+++ back/game-app/src/game/game.gateway.ts
@@ -5,12 +5,20 @@ import {
 } from '@nestjs/websockets';
 import { GameService } from './game.service';
 import { Socket } from 'socket.io';
+import { Builder } from 'builder-pattern';
+import { ConnectGameDto } from './dto/connect-game.dto';
 
 @WebSocketGateway(10003, { namespace: 'game', cors: { origin: '*' } })
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private gameService: GameService) {}
 
-  handleConnection(client: Socket): any {}
+  handleConnection(client: Socket): any {
+    const { gameKey } = client.handshake.auth;
+
+    this.gameService.connectGame(
+      Builder(ConnectGameDto).gameKey(gameKey).client(client).build(),
+    );
+  }
 
   handleDisconnect(client: Socket): any {}
 }
