891b92aeec62475655fa56ff57b9f27a621c58e4
donghyuk <reg0145@naver.com>
2023-09-04 23:09:15 +0900

    [Feat] AbstractGame 업데이트
    - 게임 기본 오브젝트, 옵션 초기화

--- back/game-app/src/game/mode/game.abstract.ts
+++ back/game-app/src/game/mode/game.abstract.ts
@@ -1,5 +1,25 @@
 import { v4 as uuid } from 'uuid';
+import { BallOption, PaddleOption } from './object/game-object.option';
+import { GameObjects, GameScore } from './object/game-object';
 
 export abstract class AbstractGame {
-  gameId = uuid();
+  private gameId = uuid();
+  private ballOption: BallOption;
+  private paddleOption: PaddleOption;
+  private gameObjects: GameObjects;
+  private gameScore: GameScore;
+
+  constructor() {
+    this.ballOption = new BallOption();
+    this.paddleOption = new PaddleOption();
+    this.gameObjects = new GameObjects();
+    this.gameScore = new GameScore();
+
+    this.initRandomBallOption();
+  }
+
+  private initRandomBallOption() {
+    this.ballOption.ballXDirection = Math.round(Math.random()) ? 1 : -1;
+    this.ballOption.ballYDirection = Math.round(Math.random()) ? 1 : -1;
+  }
 }
