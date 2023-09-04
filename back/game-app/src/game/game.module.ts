a0602d2f0cc645298c4df6433be24d952bbaecdc
donghyuk <reg0145@naver.com>
2023-09-04 20:37:27 +0900

    [Refactor] GameModule 업데이트
    - GameUserService 추가

--- back/game-app/src/game/game.module.ts
+++ back/game-app/src/game/game.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
 import { GameGateway } from './game.gateway';
+import { GameUserService } from './gameuser/game-user.service';
 
 @Module({
   controllers: [GameController],
-  providers: [GameService, GameGateway],
+  providers: [GameService, GameGateway, GameUserService],
 })
 export class GameModule {}
