f3d7170164c6a3793d57411b4de4ee8864cb68c2
donghyuk <reg0145@naver.com>
2023-09-05 16:48:02 +0900

    [Feat] 공 이동, 충돌 체크 구현
    * play()
    - x,y 좌표로 공을 이동합니다.
    - 화면 위 아래 충돌을 체크합니다.
    - 화면 좌 우 충돌을 체크합니다. (= 득점)
    - p1 패들 충돌 체크합니다.
    - p2 패들 충돌 체크합니다.
    - 반환값으로는 라운드 진행중, 종료를 반환합니다.
    
    * initObject()
    - 게임이 시작 될 때, 라운드가 새로 시작될 때 호출됩니다.
    - 공과 패들의 좌표를 초기화합니다.
    - 공의 방향이 랜덤하게 초기화됩니다.

--- back/game-app/src/game/mode/game.abstract.ts
+++ back/game-app/src/game/mode/game.abstract.ts
@@ -1,25 +1,93 @@
 import { v4 as uuid } from 'uuid';
-import { BallOption, PaddleOption } from './object/game-object.option';
+import {
+  BallOption,
+  PaddleOption,
+  SCREEN_HEIGHT,
+  SCREEN_WIDTH,
+} from './object/game-object.option';
 import { GameObjects, GameScore } from './object/game-object';
+import { GamePlayResult } from './enums/round.eunm';
 
 export abstract class AbstractGame {
-  private gameId = uuid();
+  public objects: GameObjects;
+  public score: GameScore;
+  public gameId = uuid();
   private ballOption: BallOption;
   private paddleOption: PaddleOption;
-  private gameObjects: GameObjects;
-  private gameScore: GameScore;
 
   constructor() {
     this.ballOption = new BallOption();
     this.paddleOption = new PaddleOption();
-    this.gameObjects = new GameObjects();
-    this.gameScore = new GameScore();
+    this.objects = new GameObjects();
+    this.score = new GameScore();
 
-    this.initRandomBallOption();
+    this.initObject();
   }
 
-  private initRandomBallOption() {
-    this.ballOption.ballXDirection = Math.round(Math.random()) ? 1 : -1;
-    this.ballOption.ballYDirection = Math.round(Math.random()) ? 1 : -1;
+  play() {
+    /* 공 이동 */
+    this.objects.b.x += this.ballOption.speed * this.ballOption.xDirection;
+    this.objects.b.y += this.ballOption.speed * this.ballOption.yDirection;
+
+    /* 화면 위 아래 충돌 체크 */
+    if (this.objects.b.y <= 0 + this.ballOption.radius) {
+      this.ballOption.yDirection *= -1;
+    }
+    if (this.objects.b.y >= SCREEN_HEIGHT - this.ballOption.radius) {
+      this.ballOption.yDirection *= -1;
+    }
+
+    /* 화면 좌 우 충돌 체크 (= 득점) */
+    if (this.objects.b.x <= 0) {
+      this.score.p1.score += 1;
+      this.initObject();
+      return GamePlayResult.ROUND_END;
+    }
+    if (this.objects.b.x >= SCREEN_WIDTH) {
+      this.score.p2.score += 1;
+      this.initObject();
+      return GamePlayResult.ROUND_END;
+    }
+
+    /* p1 패들 충돌 체크 */
+    if (
+      this.objects.b.x <=
+      this.objects.p1.x + this.paddleOption.width + this.ballOption.radius
+    ) {
+      if (
+        this.objects.b.y > this.objects.p1.y &&
+        this.objects.b.y < this.objects.p1.y + this.paddleOption.height
+      ) {
+        this.objects.b.x = this.paddleOption.width - this.ballOption.radius - 1;
+        this.ballOption.xDirection *= -1;
+        this.ballOption.speed += 0.05;
+      }
+    }
+    /* p2 패들 충돌 체크 */
+    if (this.objects.b.x >= this.objects.p2.x) {
+      if (
+        this.objects.b.y > this.objects.p2.y &&
+        this.objects.b.y < this.objects.p2.y + this.paddleOption.height
+      ) {
+        this.objects.b.x = this.objects.p2.x + this.ballOption.radius - 1;
+        this.ballOption.xDirection *= -1;
+        this.ballOption.speed += 0.05;
+      }
+    }
+
+    return GamePlayResult.ROUND_PROGRESS;
+  }
+
+  isGameOver;
+  private initObject() {
+    this.objects.p1.x = 0;
+    this.objects.p1.y = 0;
+    this.objects.p2.x = SCREEN_WIDTH - this.paddleOption.width;
+    this.objects.p2.y = SCREEN_HEIGHT - this.paddleOption.height;
+    this.objects.b.x = SCREEN_WIDTH / 2;
+    this.objects.b.y = SCREEN_HEIGHT / 2;
+
+    this.ballOption.xDirection = Math.round(Math.random()) ? 1 : -1;
+    this.ballOption.yDirection = Math.round(Math.random()) ? 1 : -1;
   }
 }
