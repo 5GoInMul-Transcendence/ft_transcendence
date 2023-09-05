6281faeff01b80b35bc23a9027cbf812955269e6
donghyuk <reg0145@naver.com>
2023-09-05 17:57:10 +0900

    [Feat] startGame 메시지 구독 구현

--- back/game-app/src/game/game.gateway.ts
+++ back/game-app/src/game/game.gateway.ts
@@ -9,6 +9,7 @@ import { Socket } from 'socket.io';
 import { Builder } from 'builder-pattern';
 import { ConnectGameDto } from './dto/connect-game.dto';
 import { ReadyGameDto } from './dto/ready-game.dto';
+import { StartGameDto } from './dto/start-game-dto';
 
 @WebSocketGateway(10003, { namespace: 'game', cors: { origin: '*' } })
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -37,5 +38,12 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
     const { gameKey } = client.handshake.auth;
 
     this.gameService.readyGame(Builder(ReadyGameDto).gameKey(gameKey).build());
+  }
+
+  @SubscribeMessage('startGame')
+  startGame(client: any) {
+    const { gameKey } = client.handshake.auth;
+
+    this.gameService.startGame(Builder(StartGameDto).gameKey(gameKey).build());
   }
 }
