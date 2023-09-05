664b25951c5951c3c2363e43515246503e34f718
donghyuk <reg0145@naver.com>
2023-09-05 17:25:43 +0900

    [Feat] GameCore 업데이트
    - GameCore는 GameProcessUnit의 메서드를 주기적으로 실행합니다.
    - 게임 라운드가 종료되면 게임은 일시 중단되어야합니다. 그래서 현재 연결리스트에서 제거해주는 작업을 해주고 있습니다.
    
    * this.deleteProcess[i].remove();
    - 현재 연결된 토큰을 연결리스트에서 삭제합니다.

--- back/game-app/src/game/core/game.core.ts
+++ back/game-app/src/game/core/game.core.ts
@@ -1,17 +1,32 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import LinkedList from 'fast-linked-list';
+import LinkedList, { Token } from 'fast-linked-list';
 import { GameProcessUnit } from '../game-process-unit';
+import { ProcessStatus } from './enums/process-status.enum';
 
 @Injectable()
 export class GameCore implements OnModuleInit {
   private processList: LinkedList;
+  private deleteProcess: Token[];
 
   constructor() {
     this.processList = new LinkedList();
   }
+
   onModuleInit(): any {
     setInterval(() => {
-      this.processList.forEach((processUnit, tok) => {});
+      this.processList.forEach(
+        (processUnit: GameProcessUnit, tok: Token<GameProcessUnit>) => {
+          if (processUnit.playGameByOneFrame() == ProcessStatus.END) {
+            this.deleteProcess.push(tok);
+          }
+        },
+      );
+
+      if (this.deleteProcess.length) {
+        for (let i = 0; i < this.deleteProcess.length; i++) {
+          this.deleteProcess[i].remove();
+        }
+      }
     }, 16);
   }
 
