fb61ff45bc2a1a452796290614b2542f81cc09eb
brixxt27 <brixxt27@gmail.com>
2023-09-06 18:34:09 +0900

    chore: HashService를 providers에 등록하는 것보다 HashModule을import함#188
    - 모듈끼리의 의존성을 낮추는 목적으로 다른 모듈에서 HashService 를
      사용할 때 모듈 자체를 import하지 않고, providers 에 등록하여 사용하는
    방식을 택했었다. 그러나 이는 HashService 가 확장될 때 추가적인 작업이
    많이 필요하게 된다. 예를 들어 HashService 에서 다른 providers를 주입
    받았다고 하자. 그럴 때 해당 provider 도 전부 주입 받아야 한다. 그러나 이
    provider 를 import 하고 있는 모듈 자체를 import 하면 그 때마다 주입
    받기 위한 사전 작업을 하지 않아도 된다.
    - 또한 모듈끼리는 모두 다른 컨테이너에서 빌드 된다. 즉 HashService를
      모두 따로 providers 에 등록하게 되면 각기 다른 HashService 인스턴스가
    생성된다. 때문에 의존성 관리는 될지 몰라도 메모리 상에는 비효율적이다.
    실제로 NestJS 개발자가 이를 어떻게 의도하고 만들었는지 찾아봐야 할 것
    같다.

--- back/web-app/src/login/login.module.ts
+++ back/web-app/src/login/login.module.ts
@@ -6,20 +6,20 @@ import { FtStrategy } from './ft.strategy';
 import { UserModule } from 'src/users/user/user.module';
 import { SessionModule } from 'src/session/session.module';
 import { SignupService } from 'src/signup/signup.service';
-import { HashService } from 'src/common/hash/hash.service';
+import { HashModule } from 'src/common/hash/hash.module';
 
 @Module({
   imports: [
     ConfigModule,
     UserModule,
     SessionModule,
+    HashModule,
   ],
   controllers: [LoginController],
   providers: [
     LoginService,
     FtStrategy,
     SignupService,
-    HashService,
   ]
 })
 export class LoginModule {}
