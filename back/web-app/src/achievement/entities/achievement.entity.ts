57d3ece424d993882e08788ccc71262742a68141
donghyuk <reg0145@naver.com>
2023-09-08 21:50:28 +0900

    [Feat] AchievementEntity 구현 ERD 설계와 동일하게 구현하였습니다.
    * gameWin
    - 유저의 GameWin 업적 정보을 저장하는 칼럼입니다.
    
    * achievements
    - 유저가 획득한 업적 아이디들을 저장하는 칼럼입니다.

---
+++ back/web-app/src/achievement/entities/achievement.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { AchievementGrade } from '../achievement-grade';
+
+@Entity('achievement')
+export class Achievement {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'integer', name: 'user_id' })
+  userId: number;
+
+  @Column({ type: 'json', name: 'game_win', default: { level: 0, point: 0 } })
+  gameWin: AchievementGrade;
+
+  @Column({ type: 'integer', array: true, default: [] })
+  achievements: string[];
+}
