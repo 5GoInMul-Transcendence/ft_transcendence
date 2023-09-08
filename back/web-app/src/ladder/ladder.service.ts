053cbf166c1d42f3ce0a520aa9d0246dc145a5e3
donghyuk <reg0145@naver.com>
2023-09-08 17:14:25 +0900

    [Feat] LadderService 구현 (+)
    * updateLadder()
    - 게임 결과에 따라 레더 정보를 업데이트합니다.
    - win, lose를 업데이트합니다.
    - level을 업데이트합니다. (레더 레벨 테이블 참고)

--- back/web-app/src/ladder/ladder.service.ts
+++ back/web-app/src/ladder/ladder.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Ladder } from './entities/ladder.entity';
 import { Repository } from 'typeorm';
 import { FindLadderDto } from './dto/find-ladder.dto';
+import { LadderLevelTable } from './ladder-level-table';
+import { UpdateLadderDto } from './dto/update-ladder.dto';
 
 @Injectable()
 export class LadderService {
@@ -20,5 +22,38 @@ export class LadderService {
     }
 
     return ladder;
+  }
+
+  async updateLadder(dto: UpdateLadderDto) {
+    const ladder = await this.ladderRepository.findOneBy({
+      userId: dto.userId,
+    });
+
+    if (!ladder) {
+      throw new HttpException('래더 정보를 찾을 수 없습니다.', HttpStatus.OK);
+    }
+
+    if (dto.isWin) {
+      ladder.win += 1;
+
+      const updatedPoint = ladder.win - ladder.lose;
+
+      if (updatedPoint >= LadderLevelTable[ladder.level].nextLevelPoint) {
+        ladder.level += 1;
+      }
+    }
+
+    if (dto.isWin == false) {
+      ladder.lose += 1;
+
+      const updatedPoint = ladder.win - ladder.lose;
+      const prevLevel = Math.max(0, ladder.level - 1);
+
+      if (updatedPoint < LadderLevelTable[prevLevel].nextLevelPoint) {
+        ladder.level = prevLevel;
+      }
+    }
+
+    this.ladderRepository.save(ladder);
   }
 }
