6fe2df7ff260ebc63815df7fc4edf5977b6ca1f5
donghyuk <reg0145@naver.com>
2023-09-10 04:54:45 +0900

    [Refactor] player 수정
    * PlayerNumber 추가
    - 플레이어의 오브젝트(패들)를 업데이트할 때 사용된다.

--- back/game-app/src/game/player/enums/player.ts
+++ back/game-app/src/game/player/enums/player.ts
@@ -1,7 +1,9 @@
 import { PlayerStatus } from './player-status.enum';
 import { Socket } from 'socket.io';
+import { PlayerNumber } from '../../../../dist/game/enums/player-number.enum';
 
 export class Player {
+  number: PlayerNumber;
   status: PlayerStatus;
   client: Socket;
 }
