6d4c7706710c191a6cd0d96d1a8e9b9aa10348b5
donghyuk <reg0145@naver.com>
2023-09-10 15:46:06 +0900

    [fix] game-app에서 게임결과를 받은 후 업적 에러 수정
    Achievement.prototype을 통해 키의 문자열을 확보하지 못한다. 그래서 직접 문자여로 넣어줬습니다.

--- back/web-app/src/achievement/table/object/gaem-win-table.ts
+++ back/web-app/src/achievement/table/object/gaem-win-table.ts
@@ -1,10 +1,9 @@
 import { Builder } from 'builder-pattern';
-import { Achievement } from '../../entities/achievement.entity';
 import { AchievementElement } from '../achievement-element';
 import { AchievementTable } from '../achievement-table';
 
 export const gameWinTable: AchievementTable = new AchievementTable(
-  JSON.stringify(Achievement.prototype.gameWin),
+  'gameWin',
   [
     Builder(AchievementElement).level(0).nextLevelPoint(3).awardId('1001').build(),
     Builder(AchievementElement).level(1).nextLevelPoint(5).awardId('1002').build(),
