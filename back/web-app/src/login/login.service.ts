1b562d286c95e41358a8fe654a172dc7e8b3550e
donghyuk <reg0145@naver.com>
2023-09-13 17:22:06 +0900

    [Feat] 2fa 활성화 확인
    * checkTwoFactorOn
    - 2fa 활성화되어 있을 경우 인증코드를 전송합니다.
    - 2fa 활성화되었다면 true, 아니면 false를 반환합니다.

--- back/web-app/src/login/login.service.ts
+++ back/web-app/src/login/login.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@nestjs/common'; 
 import { TwoFactorStatus } from 'src/users/enums/twoFactor-status.enum';
+import { MemoryUserService } from '../users/memoryuser/memory-user.service';
+import { Builder } from 'builder-pattern';
+import { FindUserDto } from '../users/memoryuser/dto/find-user.dto';
+import { AuthService } from '../auth/auth.service';
+import { AuthMailDto } from '../auth/dto/auth-mail.dto';
+import { AuthPhoneDto } from '../auth/dto/auth-phone.dto';
 
 @Injectable()
 export class LoginService {
+  constructor(
+    private memoryUserService: MemoryUserService,
+    private authService: AuthService,
+  ) {}
+
+  checkTwoFactorOn(userId: number) {
+    const user = this.memoryUserService.findUserByUserId(
+      Builder(FindUserDto).userId(userId).build(),
+    );
+
+    if (user.twoFactor === TwoFactorStatus.DISABLED) {
+      return false;
+    }
+
+    if (user.twoFactor === TwoFactorStatus.MAIL) {
+      this.authService.authMail(
+        Builder(AuthMailDto).userId(user.id).mail(user.mail).build(),
+      );
+    }
+
+    if (user.twoFactor === TwoFactorStatus.PHONE) {
+      this.authService.authPhone(
+        Builder(AuthPhoneDto).userId(user.id).phone(user.phone).build(),
+      );
+    }
+
+    return true;
+  }
 
   isTwoFaOn(twoFactorStatus: TwoFactorStatus): boolean {
     return twoFactorStatus != TwoFactorStatus.DISABLED;
