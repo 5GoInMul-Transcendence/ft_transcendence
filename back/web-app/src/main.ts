e55d7ce95744035b3e0784b2b8b9812d6d4ad202
donghyuk <reg0145@naver.com>
2023-09-13 14:02:27 +0900

    [Feat] main.ts 업데이트
    * useGlobalPipes 추가
    - 유효성 검사가 진행될 수 있도록 글로벌 파이프를 추가합니다. 이렇게 되면 클라이언트 요청시 globalPipe를 거쳐가게 되고 정의한 데코레이터가 실행됩니다.

--- back/web-app/src/main.ts
+++ back/web-app/src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/response/success-or-redirection/response.interceptor';
 import { HttpExceptionFilter } from './common/response/fail/http-exception.filter';
 import { corsOptions } from './common/cors/cors.options';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(corsOptions);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(8080);
