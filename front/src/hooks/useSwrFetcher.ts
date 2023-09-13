979b431cb86f957819ba26ff4a211630e0645d60
arkingco <arkingco@gmail.com>
2023-09-13 19:02:17 +0900

    fix : login error handling #221

--- front/src/hooks/useSwrFetcher.ts
+++ front/src/hooks/useSwrFetcher.ts
@@ -2,14 +2,7 @@ import useSwr from 'swr';
 import fetcher from '@/utils/fetcher';
 
 const useSwrFetcher = <T>(api: string): T | undefined => {
-  const { data } = useSwr(api, fetcher);
-
-  if (
-    data?.resStatus.code !== '0000' &&
-    data?.resStatus.code !== '1000' &&
-    data?.resStatus.code !== '2000'
-  )
-    return undefined;
+  const { data, error } = useSwr(api, fetcher);
 
   return data?.data;
 };
--- front/src/utils/AxiosApiHadling.tsx
+++ front/src/utils/AxiosApiHadling.tsx
@@ -13,9 +13,32 @@ export default function AxiosApiHadling() {
         return response;
       },
       (error) => {
+        const resData = error.response.data.data;
+        const resStatus = error.response.data.resStatus.code;
         const resMessage = error.response.data.resStatus.message;
-        if (modal === null) setModal({ type: 'API-Error' });
-        setInvalidMsg(resMessage);
+        switch (resStatus) {
+          case '0001':
+            if (modal === null) setModal({ type: 'API-Error' });
+            setInvalidMsg(resMessage);
+            break;
+          case '0002':
+            window.location.href = `http://localhost:3000${resData}`;
+            break;
+          case '1001':
+            if (modal === null) setModal({ type: 'API-Error' });
+            setInvalidMsg(resMessage);
+            break;
+          case '1002':
+            window.location.href = `http://localhost:3000${resData}`;
+            break;
+          case '2001':
+            if (modal === null) setModal({ type: 'API-Error' });
+            setInvalidMsg(resMessage);
+            break;
+          case '2002':
+            window.location.href = `http://localhost:3000${resData}`;
+            break;
+        }
       }
     );
 
