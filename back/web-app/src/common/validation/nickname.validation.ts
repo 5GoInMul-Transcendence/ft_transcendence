693d52a2f907ecddef470c6f801fa8ef698d285a
donghyuk <reg0145@naver.com>
2023-09-13 14:00:30 +0900

    [Feat] 닉네임 유효성 검사
    * 데코레이터 기반 유효성 검사
    - nest에서 제공해주는 'class-validator' 라이브러리를 사용하여 데코레이터를 통해 유효성 검사를 구현할 수 있습니다.
    
    * IsNicknameConstraint
    - 데코레이터 검사기
    - @ValidatorConstraint()
      class-validator에서 제공하는 데코레이터입니다. 유효성 검사 클래스를 만들 때 사용됩니다.
    - ValidatorConstraintInterface
      이 또한 class-validator에서 제공하는 인터페이스입니다. 두 개의 추상 메서드를 가지고 있습니다.
      - validate()
        유효성을 검사하고 true/false값을 반환합니다.
      - defaultMessage()
        유효성 검사가 실패할 경우 반환할 message입니다.
    
    * IsNickname
    - 데코레이터 팩토리로, 해당 함수를 호출하면 실제로 작동하는 데코레이터 함수가 반환됩니다.
    - registerDecorator()
      - 데코레이터의 옵션을 등록합니다.
      - target: 유효성 검사를 적용할 대상입니다. (저희는 reqDto 객체가 되겠네요.) object.constructor는 해당 함수를 호출한 객체를 가리킵니다.
      - async: 비동기 처리 유무
      - propertyName: 유효성 검사를 진행할 객체의 프로퍼티 이름입니다.
      - options: 데코레이터에 정의한 유효성 옵션을 가리킵니다. 이는 파라미터로 전달되며 현재는 아무런 값도 사용하지 않습니다.
      - validator: 위에서 정의한 데코레이터 검사기를 지정합니다.
      - constraints: 더 필요한 제약 조건들을 배열 요소로 추가합니다.

---
+++ back/web-app/src/common/validation/nickname.validation.ts
@@ -0,0 +1,32 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint()
+export class IsNicknameConstraint implements ValidatorConstraintInterface {
+  private readonly pattern: RegExp = /^[A-Za-z0-9]{2,12}$/;
+
+  validate(nickname: string): boolean {
+    return this.pattern.test(nickname);
+  }
+
+  defaultMessage(): string {
+    return '유효하지 않은 닉네임입니다.';
+  }
+}
+
+export function IsNickname(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      async: false,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: IsNicknameConstraint,
+      constraints: [],
+    });
+  };
+}
