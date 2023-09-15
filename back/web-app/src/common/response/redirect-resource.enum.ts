b9d55d09d716495a367365443d9ce9fbd690fb79
donghyuk <reg0145@naver.com>
2023-09-16 06:49:57 +0900

    [Refactor] 상대방 프로필 조회 업데이트
    * 추가된 기능
    - 상대방이 나라면 마이 프로필로 리다이렉트 시킵니다.
    
    * 목데어티 -> 실제 데이터 반환

--- back/web-app/src/common/response/redirect-resource.enum.ts
+++ back/web-app/src/common/response/redirect-resource.enum.ts
@@ -2,4 +2,5 @@ export enum RedirectResource {
   MAIN = '/main',
   LOGIN = '/login',
   TWOFACTOR = '/2fa',
+  MYPROFILE = 'myprofile',
 }
--- back/web-app/src/users/user/user.controller.ts
+++ back/web-app/src/users/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Param, Session } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Res,
+  Session,
+} from '@nestjs/common';
 import { GetUserProfileByNicknameReqDto } from './dto/get-user-profile-by-nickname-req.dto';
 import { FindUserDto } from '../memoryuser/dto/find-user.dto';
 import { FindUserByNicknameDto } from '../memoryuser/dto/find-user-by-nickname.dto';
@@ -6,29 +13,53 @@ import { GameRecordDto } from '../me/dto/game-record.dto';
 import { GetUserProfileByNicknameResDto } from './dto/get-user-profile-by-nickname-res.dto';
 import { Builder } from 'builder-pattern';
 import { MemoryUserService } from '../memoryuser/memory-user.service';
-
+import { AchievementService } from '../../achievement/achievement.service';
+import { LadderService } from '../../ladder/ladder.service';
+import { ApiResponseForm } from '../../common/response/api-response-form';
+import { RedirectResource } from '../../common/response/redirect-resource.enum';
+import { Response } from 'express';
+import { FindLadderDto } from '../../ladder/dto/find-ladder.dto';
+import { FindAchievementDto } from '../../achievement/dto/find-achievement.dto';
 @Controller('user')
 export class UserController {
-  constructor(private memoryUserService: MemoryUserService) {}
+  constructor(
+    private memoryUserService: MemoryUserService,
+    private ladderService: LadderService,
+    private achievementService: AchievementService,
+  ) {}
 
   @Get(':nickname')
-  getUserProfileByNickname(
+  async getUserProfileByNickname(
     @Session() session: Record<string, any>,
+    @Res() res: Response,
     @Param() dto: GetUserProfileByNicknameReqDto,
   ) {
     const me = this.memoryUserService.findUserByUserId(
       Builder(FindUserDto).userId(session.userId).build(),
     );
+
+    if (me.nickname === dto.nickname) {
+      res.status(HttpStatus.FOUND);
+      return ApiResponseForm.redirect(RedirectResource.MYPROFILE);
+    }
+
     const findUser = this.memoryUserService.findUserByNickname(
       Builder(FindUserByNicknameDto).nickname(dto.nickname).build(),
     );
+    const ladder = await this.ladderService.findLadderByUserId(
+      Builder(FindLadderDto).userId(findUser.id).build(),
+    );
+    const achievement = await this.achievementService.findAchievementByUserId(
+      Builder(FindAchievementDto).userId(findUser.id).build(),
+    );
 
     const gameRecodeDto = Builder(GameRecordDto)
-      .win(10)
-      .lose(10)
-      .ladderLevel(10)
-      .achievement(['0123456789abcdef', '0123456789abcdef'])
+      .ladderLevel(ladder.level)
+      .win(ladder.win)
+      .lose(ladder.lose)
+      .achievement(achievement.achievements)
       .build();
+
     return Builder(GetUserProfileByNicknameResDto)
       .id(findUser.id)
       .nickname(findUser.nickname)
