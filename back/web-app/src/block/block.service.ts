0f73cf7ee5d61a5fb91546af46058d3962f18789
donghyuk <reg0145@naver.com>
2023-09-19 18:13:05 +0900

    [Feat] blockService
    * block entity
    - id: number;
      userId: number;
      blocks: number[];
    
    * block()
    - 상대방을 블럭 또는 논블럭합니다.
    - 메모리 유저를 통해 현재 블럭 상태를 확인합니다.
      - 블럭 중이었다면, 블럭을 풀고 메모리, DB에 저장합니다.
      - 블락이 아니라면, 블럭을 설정하고 메모리, DB에 저장합니다.
    - 그리고 블록 여부를 반환합니다.

---
+++ back/web-app/src/block/block.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { MemoryUserService } from '../users/memoryuser/memory-user.service';
+import { FindUserDto } from '../users/memoryuser/dto/find-user.dto';
+import { Builder } from 'builder-pattern';
+import { BlockDto } from './dto/block.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Block } from './block.entity';
+
+@Injectable()
+export class BlockService {
+  constructor(
+    @InjectRepository(Block)
+    private blockRepository: Repository<Block>,
+    private memoryUserService: MemoryUserService,
+  ) {}
+
+  block(dto: BlockDto): boolean {
+    const { userId, blockUserId } = dto;
+
+    const user = this.memoryUserService.findUserByUserId(
+      Builder(FindUserDto).userId(userId).build(),
+    );
+    // BlockUser가 없을 경우 예외를 반환합니다.
+    this.memoryUserService.findUserByUserId(
+      Builder(FindUserDto).userId(blockUserId).build(),
+    );
+
+    let isBlocked;
+    if (user.blocks.has(blockUserId)) {
+      user.blocks.delete(blockUserId);
+      isBlocked = false;
+    } else {
+      user.blocks.add(blockUserId);
+      isBlocked = true;
+    }
+
+    this.blockRepository.update(
+      { userId: userId },
+      { blocks: Array.from(user.blocks) },
+    );
+
+    return isBlocked;
+  }
+}
